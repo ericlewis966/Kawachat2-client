2d4834f23814f9cb4a75fb62396dbfa424906bba
storm <82670106+cyber-storm-200712@users.noreply.github.com>
2023-02-14 21:24:28 +0900

    Create karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -20,7 +20,7 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
     },
     angularCli: {
-      environment: 'dev'
+      environment: 'pro'
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
